dbe69d81d3be48fa3ab1b821caaa6b3890ae1717
Tomasz Krzysztof Sobota <tkso@netcompany.com>
2019-08-09 18:32:21 +0200

    First microservice: HTTP Transmitter

--- src/main/java/microservices/comunication/test/TestApplication.java
+++ src/main/java/microservices/comunication/test/TestApplication.java
@@ -1,13 +1,46 @@
 package microservices.comunication.test;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import microservices.comunication.test.http.HttpServiceTransmitter;
+import microservices.comunication.test.registration.RegistrationServer;
 
 @SpringBootApplication
 public class TestApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(TestApplication.class, args);
+        String serverName = "NO-VALUE";
+
+        switch (args.length) {
+            case 2:
+                // Optionally set the HTTP port to listen on, overrides
+                // value in the <server-name>-server.yml file
+                System.setProperty("server.port", args[1]);
+                // Fall through into ..
+
+            case 1:
+                serverName = args[0].toLowerCase();
+                break;
+
+            default:
+                usage();
+                return;
+        }
+
+        if (serverName.equals("registration") || serverName.equals("reg")) {
+            RegistrationServer.main(args);
+        } else if (serverName.equals("accounts")) {
+            HttpServiceTransmitter.main(args);
+        } else {
+            System.out.println("Unknown server type: " + serverName);
+            usage();
+        }
+    }
+
+    protected static void usage() {
+        System.out.println("Usage: java -jar ... <server-name> [server-port]");
+        System.out.println(
+                "     where server-name is 'reg', 'registration', " + "'accounts' or 'web' and server-port > 1024");
     }
 
 }
